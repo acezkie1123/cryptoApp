5827e7db80fd438ec1b4e4cf3857028b6c9fb8db
Albert <acecanosa@gmail.com>
2022-05-06 20:31:14 +0800

    first commit

---
+++ src/BackgroundAnimate.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BackgroundAnimate = () => {
+  return (
+   <div class="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+  )
+}
+
+export default BackgroundAnimate
