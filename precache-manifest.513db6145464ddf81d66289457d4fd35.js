5360d9e9b38ff80bafc1044d9c54f2997eaf5739
Albert <acecanosa@gmail.com>
2022-05-06 20:55:26 +0800

    Updates

---
+++ precache-manifest.513db6145464ddf81d66289457d4fd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be027c8deb9cebe3ebca",
+    "url": "/cryptoApp/static/css/main.77ebc243.chunk.css"
+  },
+  {
+    "revision": "be027c8deb9cebe3ebca",
+    "url": "/cryptoApp/static/js/main.be027c8d.chunk.js"
+  },
+  {
+    "revision": "97bc1625e33b04eb7828",
+    "url": "/cryptoApp/static/js/1.97bc1625.chunk.js"
+  },
+  {
+    "revision": "09d79a8a8086ff333446",
+    "url": "/cryptoApp/static/js/2.09d79a8a.chunk.js"
+  },
+  {
+    "revision": "30fae1c595efe4f7248d",
+    "url": "/cryptoApp/static/js/runtime~main.30fae1c5.js"
+  },
+  {
+    "revision": "9130fbe105ef4576a091bf6d6fa88b27",
+    "url": "/cryptoApp/index.html"
+  }
+];
